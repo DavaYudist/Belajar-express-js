abe374a4c46bc720b3a7ad329a4e19f148e583e1
DAVANA YUDIST <davayudist@gmail.com>
2026-03-05 08:21:05 +0700

    update project to module 4

--- routes/kategori.js
+++ routes/kategori.js
@@ -11,7 +11,7 @@ router.get("/", function (req, res, next) {
   connection.query(query, function(err, result){
     if(err){
       console.error("ERORR : ", err);
-      res.status(500).send("Error");
+      return res.status(500).send("Error" + er);
 
     }
 
@@ -20,6 +20,24 @@ router.get("/", function (req, res, next) {
       kategori: result
     })
   })
+});
+
+router.get('/create', function(req, res, next) {
+    res.render('kategori/create', {
+        title: 'Tambah Kategori'
+    });
+});
+
+router.post('/store', function(req, res, next) {
+    const nama_kategori = req.body.nama_kategori;
+    const query = "INSERT INTO kategori (nama_kategori) VALUES (?)";
+    connection.query(query, [nama_kategori], function(err, result) {
+        if (err) {
+            console.error("ERROR : ", err);
+            return res.status(500).send("Terjadi kesalahan pada database.");
+        } 
+        res.redirect("/kategori");
+    });
 });
 
 module.exports = router;
